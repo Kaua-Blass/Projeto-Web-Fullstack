3e209c7e0bd92ffc8f2701868e7cf54d87c3694d
kauablass17 <kauablass17@gmail.com>
2025-10-08 13:51:38 -0300

    ajuste imagens

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,22 @@
 // app/page.js
 'use client'; 
+import Image from 'next/image';
+
 
 const produtosMockados = [
-    { id: 1, nome: "GARRAFA TÉRMICA QUENCHER", preco: 109.90, pix: 103.90, imagemUrl: "garrafa1.png" },
-    { id: 2, nome: "GARRAFA TÉRMICA AÇO INOX", preco: 77.90, pix: 73.90, imagemUrl: "garrafa2.png" },
-    { id: 3, nome: "GARRAFA TÉRMICA DIGITAL", preco: 49.99, pix: 44.90, imagemUrl: "garrafa3.png" },
-    { id: 4, nome: "GARRAFA TÉRMICA 1L LE COFFEE", preco: 119.90, pix: 113.90, imagemUrl: "garrafa4.png" },
+    { id: 1, nome: "GARRAFA TÉRMICA QUENCHER", preco: 109.90, pix: 103.90, imagemUrl: "quencher.png" },
+    { id: 2, nome: "GARRAFA TÉRMICA AÇO INOX", preco: 77.90, pix: 73.90, imagemUrl: "termicainox.png" },
+    { id: 3, nome: "GARRAFA TÉRMICA DIGITAL", preco: 49.99, pix: 44.90, imagemUrl: "digital.png" },
+    { id: 4, nome: "GARRAFA TÉRMICA 1L LE COFFEE", preco: 119.90, pix: 113.90, imagemUrl: "lecoffee.png" },
 ];
+
+const lancamentosMockados = [
+  { id: 5, nome: "GARRAFA TÉRMICA STANLEY FLIP 651ML CHARCOAL", preco: 149.90, pix: 144.90, imagemUrl: "stanleyflip.png" },
+  { id: 6, nome: "GARRAFA TÉRMICA AEROLIGHT FAST FLOW MATTE BLACK | 1.1L", preco: 139.90, pix: 134.90, imagemUrl: "fastflow.png" },
+  { id: 7, nome: "GARRAFA TÉRMICA INOX 4L BLACK ERVA", preco: 79.99, pix: 74.90, imagemUrl: "termicainox.png" },
+  { id: 8, nome: "GARRAFA TÉRMICA 1 L LE COFFEE", preco: 119.90, pix: 113.90, imagemUrl: "lecoffee.png" },
+];
+
 
 export default function Home() {
   
@@ -16,12 +26,13 @@ export default function Home() {
         <div className="top-bar">
           {/* USANDO CLASSNAME */}
           <input type="text" placeholder="O que você está buscando?" className="input-busca" />
-          <img src="/IMAGENS/logo.png" alt="Logo Eu Quero" className="logo-img" /> 
+          <img src="logo.png" alt="Logo Eu Quero" className="logo-img" /> 
           <div className="user-area">
             <span onClick={() => window.location.href='/login'}>👤 Login / Cadastro</span>
             <span onClick={() => window.location.href='/carrinho'}>🛒 Carrinho(1) <strong>R$149,90</strong></span>
           </div>
         </div>
+        <div className="nav-container">
         <nav>
           <ul className="nav-list">
             <li><a href="#">Copos Térmicos</a></li>
@@ -31,35 +42,61 @@ export default function Home() {
             <li><a href="#">Personalizar</a></li>
           </ul>
         </nav>
+        </div>
       </header>
 
       <section className="product-section">
-        <h2>Destaques (Layout Fixo) <span>›</span></h2>
+        <h2>Destaques<span>›</span></h2>
         
-        <div className="product-list">
-          {produtosMockados.map(produto => (
-            <div key={produto.id} className="product-card">
-              {/* USANDO CLASSNAME */}
-              <img src={produto.imagemUrl} alt={produto.nome} className="product-img" />
-              <p className="name">{produto.nome}</p>
-              <p className="price">R$ {produto.preco.toFixed(2)}</p>
-              <p className="pix">R$ {produto.pix.toFixed(2)} à vista no Pix</p> 
-              <button className="add-button">Adicionar</button>
-            </div>
-          ))}
-        </div>
+       <div className="product-list">
+  {produtosMockados.map(produto => (
+    <div key={produto.id} className="product-card">
+      <div className="image-wrapper"> 
+          <Image 
+              src={`/${produto.imagemUrl}`} 
+              alt={produto.nome}
+              className="product-img" 
+              fill 
+              sizes="(max-width: 768px) 100vw, 33vw" 
+              style={{ objectFit: 'contain' }} 
+          />
+      </div>
+
+      <p className="name">{produto.nome}</p>
+      <p className="price">R$ {produto.preco.toFixed(2)}</p>
+      <p className="pix">R$ {produto.pix.toFixed(2)} à vista no Pix</p> 
+      <button className="add-button">Adicionar</button>
+    </div>
+  ))}
+</div>
+
       </section>
 
-      <section className="product-section">
-        <h2>Lançamentos (Layout Fixo) <span>›</span></h2>
-        
-        <div className="product-list">
-             <div className="product-card"><img src="lan1.png" alt="" className="product-img" /><p>GARRAFA STANLEY FLIP</p><p className="price">R$ 149,90</p><button className="add-button">Adicionar</button></div>
-             <div className="product-card"><img src="lan2.png" alt="" className="product-img" /><p>GARRAFA TÉRMICA AEROLIGHT</p><p className="price">R$ 139,90</p><button className="add-button">Adicionar</button></div>
-             <div className="product-card"><img src="lan3.png" alt="" className="product-img" /><p>GARRAFA TÉRMICA INOX</p><p className="price">R$ 79,99</p><button className="add-button">Adicionar</button></div>
-             <div className="product-card"><img src="lan4.png" alt="" className="product-img" /><p>GARRAFA TÉRMICA 1L</p><p className="price">R$ 119,90</p><button className="add-button">Adicionar</button></div>
+   <section className="product-section">
+  <h2>Lançamentos<span>›</span></h2>
+
+  <div className="product-list">
+    {lancamentosMockados.map(produto => (
+      <div key={produto.id} className="product-card">
+        <div className="image-wrapper"> 
+          <Image 
+            src={`/${produto.imagemUrl}`} 
+            alt={produto.nome}
+            className="product-img" 
+            fill 
+            sizes="(max-width: 768px) 100vw, 33vw" 
+            style={{ objectFit: 'contain' }} 
+          />
         </div>
-      </section>
+        <p className="name">{produto.nome}</p>
+        <p className="price">R$ {produto.preco.toFixed(2)}</p>
+        <p className="pix">R$ {produto.pix.toFixed(2)} à vista no Pix</p> 
+        <button className="add-button">Adicionar</button>
+      </div>
+    ))}
+  </div>
+</section>
+
 
     </main>
   );
